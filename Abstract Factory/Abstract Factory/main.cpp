281d5d1fb6de2e35032d59bbe22e1968f1393355
SavannahBolger <C00215747@itcarlow.ie>
2019-09-23 08:04:00 +0100

    update

--- Abstract Factory/Abstract Factory/main.cpp
+++ Abstract Factory/Abstract Factory/main.cpp
@@ -8,36 +8,44 @@
 class DrawAPI
 {
 public:
+	DrawAPI() {};
+	~DrawAPI() {};
 	virtual void Draw() = 0;
 };
 
-class DrawImplement : public DrawAPI
+class DrawImpl : public DrawAPI
 {
 public:
 	void Draw()
 	{
-		std::cout << "draw api" << std::endl;
+		std::cout << "Draw Impl" << std::endl;
 	}
 };
 
 class Character
 {
 public:
-
+	Character() {};
+	~Character() {};
+	virtual void Draw() = 0;
 };
 
 class Player : public Character
 {
 public:
 	Player(DrawAPI* api) { this->api = api; };
-
+	void Draw() { api->Draw(); }
 private:
 	DrawAPI* api;
 };
 
-
 int main()
 {
+	DrawAPI* api = new DrawImpl();
+	Character* character = new Player(api);
+	character->Draw();
+	std::cin.get();
 
+	system("pause");
 	return 0;
 }
